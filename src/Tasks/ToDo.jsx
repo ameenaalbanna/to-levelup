43c0a92f2cb30b117d2f663008da0131e6f83635
Ameena Albanna <ameena.albanna@gmail.com>
2025-08-01 00:29:18 +0300

    ToDo interaction done

--- src/Tasks/ToDo.jsx
+++ src/Tasks/ToDo.jsx
@@ -1,26 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
+import XPPopup from '../Components/XPPopup';
 
 function ToDo() {
+    const [todos, setTodos] = useLocalStorage('todos', []);
+    const [inputValue, setInputValue] = useState('');
+    const [selectedDate, setSelectedDate] = useState('');
+    const [xp, setXp] = useLocalStorage('xp', 0);
+    const [showPopup, setShowPopup] = useState(false);
+    const [xpChange, setXpChange] = useState(0);
+
+    const formatDate = (dateStr) => {
+        const date = new Date(dateStr);
+        return isNaN(date)
+            ? ''
+            : `${date.getDate()}/${date.toLocaleString('default', { month: 'short' })}/${date.getFullYear()}`;
+    };
+
+    const addTodo = () => {
+        if (!inputValue.trim()) return;
+        const newTodo = {
+            id: Date.now(),
+            text: inputValue.trim(),
+            completed: false,
+            date: selectedDate
+        };
+        setTodos([...todos, newTodo]);
+        setInputValue('');
+        setSelectedDate('');
+    };
+
+    const deleteTodo = (id) => {
+        setTodos(todos.filter(t => t.id !== id));
+    };
+
+    const toggleComplete = (id) => {
+        const updated = todos.map(todo => {
+            if (todo.id === id) {
+                const newCompleted = !todo.completed;
+                const xpDelta = newCompleted ? 15 : -15;
+                setXp(prev => prev + xpDelta);
+                setXpChange(xpDelta);
+                setShowPopup(true);
+                return { ...todo, completed: newCompleted };
+            }
+            return todo;
+        });
+        setTodos(updated);
+    };
+
     return (
         <div className="card to-dos">
             <div className='section-title'>
                 <h3>TO_DOS</h3>
-                <label className="add-task" style={{ width: "100%" }}><input type='text' className='todo-input' placeholder='Add a to-do' />
+                <label className="add-task" style={{ width: "100%" }}>
+                    <input
+                        type='text'
+                        className='todo-input'
+                        placeholder='Add a to-do'
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        onKeyDown={(e) => e.key === 'Enter' && addTodo()}
+                    />
+                    <button className="btn-purple" onClick={addTodo}>Add</button>
                     <span className="datepicker-toggle">
                         <span className="datepicker-toggle-button btn-purple">
-                            <img src='assets/images/calendar.svg' />
+                            <img src='assets/images/calendar.svg' alt="calendar" />
                         </span>
-                        <input type="date" className="datepicker-input" />
+                        <input
+                            type="date"
+                            className="datepicker-input"
+                            value={selectedDate}
+                            onChange={(e) => setSelectedDate(e.target.value)}
+                        />
                     </span>
                 </label>
             </div>
             <ul className="task-list">
-                <li className="task-item"><label><input type="checkbox" />Submit Graduation project</label><span className='date-gray'>1/Aug/2025</span></li>
-                <li className="task-item"><label><input type="checkbox" />Post in LinkedIn</label><span className='date-gray'>1/Aug/2025</span></li>
-                <li className="task-item"><label><input type="checkbox" />Manage the posts in the website</label><span className='date-gray'>1/Aug/2025</span></li>
+                {todos.map(todo => (
+                    <li key={todo.id} className={`task-item ${todo.completed ? 'task-completed' : ''}`}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={todo.completed}
+                                onChange={() => toggleComplete(todo.id)}
+                            />
+                            {todo.text}
+                        </label>
+                        <span className='task-date'>{formatDate(todo.date)}</span>
+                        <button className="btn-gray" onClick={() => deleteTodo(todo.id)}>
+                            <img src='assets/images/trash.svg' alt="delete" />
+                        </button>
+                    </li>
+                ))}
             </ul>
+
+            {showPopup && (
+                <XPPopup
+                    value={xpChange}
+                    onComplete={() => setShowPopup(false)}
+                />
+            )}
         </div>
-    )
+    );
 }
 
-export default ToDo
+export default ToDo;
