05f5c96f26d9803a22c028af878b8ea286e263f5
Ameena Albanna <ameena.albanna@gmail.com>
2025-08-02 04:17:42 +0300

    Loca API created Images added to folder style changes

--- src/Rewards/Rewards.jsx
+++ src/Rewards/Rewards.jsx
@@ -6,7 +6,7 @@ function Rewards() {
             <div className='reward-container'>
                 <div className='reward-item card'>
                     <img src="assets/images/sun.png"/>
-                    <label>Reward's Name</label>
+                    <label><span>Reward's Name</span> | <span></span>coins</label>
                     <button className='btn-purple'>BUY</button>
                 </div>
             </div>
